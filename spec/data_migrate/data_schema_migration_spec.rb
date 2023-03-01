73c3f506e6dc6707c52345128b7cfb73cfe1609f
Jack Jennings <jack.jennings@leafly.com>
2023-02-28 20:05:55 -0800

    Add configuration for table name

--- spec/data_migrate/data_schema_migration_spec.rb
+++ spec/data_migrate/data_schema_migration_spec.rb
@@ -6,6 +6,27 @@ describe DataMigrate::DataSchemaMigration do
     it "returns correct table name" do
       expect(subject.table_name).to eq("data_migrations")
     end
+
+    describe "data migrations table name configured" do
+      let(:data_migrations_table_name) { "my_app_data_template_migrations"}
+
+      before do
+        @before = DataMigrate.config.data_migrations_table_name
+        DataMigrate.configure do |config|
+          config.data_migrations_table_name = data_migrations_table_name
+        end
+      end
+
+      after do
+        DataMigrate.configure do |config|
+          config.data_migrations_table_name = @before
+        end
+      end
+
+      it "returns correct table name" do
+        expect(subject.table_name).to eq(data_migrations_table_name)
+      end
+    end
   end
 
   describe :index_name do
